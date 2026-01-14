132032639bc4d47a878e8b63424b1028789781ea
NetraAcademic <netraacademics@gmail.com>
2026-01-14 13:39:04 +0300

    Update articles.js

--- js/articles.js
+++ js/articles.js
@@ -1,7 +1,5 @@
 import { db } from "./firebase-config.js";
 import { collection, query, where, getDocs, orderBy } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
-import { deleteDoc, doc } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
-
 
 const articlesList = document.getElementById("articles-list");
 
@@ -22,31 +20,16 @@ async function loadApprovedArticles() {
             const div = document.createElement("div");
             div.classList.add("article-card");
 
-            div.innerHTML = `
-                <h3>${article.title}</h3>
-                <p class="article-meta">
-                    By ${article.authorName} | ${article.createdAt?.toDate().toLocaleString() || ""}
-                </p>
-                <p class="article-preview">
-                    ${article.content.substring(0, 200)}${article.content.length > 200 ? "..." : ""}
-                </p>
-                <button class="delete-btn">Sil</button>
-            `;            
-
             // Tüm kart tıklanabilir olsun
             div.onclick = () => {
                 window.location.href = `article.html?id=${docSnap.id}`;
             };
-            
-            const deleteBtn = div.querySelector(".delete-btn");
-            deleteBtn.onclick = (e) => {
-                e.stopPropagation();
-                deleteArticle(docSnap.id);
-            };
 
-
-
-
+            div.innerHTML = `
+                <h3>${article.title}</h3>
+                <p class="article-meta">By ${article.authorName} | ${article.createdAt?.toDate().toLocaleString() || ""}</p>
+                <p class="article-preview">${article.content.substring(0, 200)}${article.content.length > 200 ? "..." : ""}</p>
+            `;
 
             articlesList.appendChild(div);
         });
@@ -55,18 +38,6 @@ async function loadApprovedArticles() {
         articlesList.innerHTML = "<p>Articles yüklenemedi: " + err.message + "</p>";
     }
 }
-
-    window.deleteArticle = async function (id) {
-        if (!confirm("Bu yazıyı silmek istiyor musun?")) return;
-
-        try {
-            await deleteDoc(doc(db, "articles", id));
-            alert("Yazı silindi");
-            loadApprovedArticles(); // listeyi yenile
-        } catch (err) {
-            alert("Silme hatası: " + err.message);
-        }
-    };
 
 // Sayfa açıldığında yükle
 loadApprovedArticles();
